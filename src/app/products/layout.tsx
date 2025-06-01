5173308c38249b61e45b0b306b0b16e2b4327350
khalid21456 <khalidedaoudi8@gmail.com>
2025-06-01 22:37:16 +0100

    first commit

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,14 @@
+import "../globals.css";
+
+export default function ProductsLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <div className="h-20 bg-green-500 shadow-amber-200"></div>
+            <div>{children}</div>
+        </>
+    )
+}
